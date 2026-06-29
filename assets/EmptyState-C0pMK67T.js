30807304408e4a6a05f6b1734680f06491f78888
Rouse <muzafarova999@icloud.com>
2026-06-29 20:00:13 +0500

    Updates

--- assets/EmptyState-C0pMK67T.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-C62idO4v.js";function c({message:e}){return s.jsxs("section",{className:"status-block shell center-screen",children:[s.jsx("div",{className:"empty-icon",children:"🙂"}),s.jsx("p",{children:e})]})}export{c as E};
